4d4eaab676f7870d2cc17cc4bc4da06aacc62308
Clément Ballet <clementballet@gmail.com>
2018-12-05 11:49:41 +0100

    Make Routes & rebuilt arbo

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,36 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { APP_CONFIG } from './app.config';
+
+import { NavbarComponent } from './navbar/navbar.component';
+import { MovieSingleComponent } from './movie-single/movie-single.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+// PAGE HOME
+import {HomeComponent} from "./home/home.component";
+import {HomeCarouselComponent} from "./home/home-carousel/home-carousel.component";
+import {HomeFiltersComponent} from "./home/home-filters/home-filters.component";
+
+import {environment} from "../environments/environment";
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        HomeComponent,
+        NavbarComponent,
+        HomeCarouselComponent,
+        HomeFiltersComponent,
+        MovieSingleComponent,
+        ShoppingCartComponent
       ],
+      imports: [
+        RouterModule
+      ],
+      providers: [
+        {provide: APP_CONFIG, useValue: environment},
+      ]
     }).compileComponents();
   }));
 
