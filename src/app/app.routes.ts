dcb44212100a17fd8526665a0c0a5d455d1662d9
Takeshi69 <qdudon@gmail.com>
2018-12-06 14:15:43 +0100

    Page About Done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,15 @@
 import { Routes } from '@angular/router';
 
-import {MovieSingleComponent} from "./movie-single/movie-single.component";
-import {ShoppingCartComponent} from "./shopping-cart/shopping-cart.component";
-import {HomeComponent} from "./home/home-global/home.component";
+import { MovieSingleComponent } from './movie-single/movie-single.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { HomeComponent } from './home/home-global/home.component';
+import { AboutComponent } from './about/about.component';
 
 export const ROUTES: Routes = [
   { path: '', component: HomeComponent },
   { path: 'movie', component: MovieSingleComponent },
   { path: 'cart', component: ShoppingCartComponent },
+  { path: 'about', component: AboutComponent},
   { path: '',
     redirectTo: '/',
     pathMatch: 'full'
