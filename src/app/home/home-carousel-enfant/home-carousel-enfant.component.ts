8326d47d17a7af2ea7e864e946970ed98dcc9682
Takeshi69 <qdudon@gmail.com>
2018-12-07 12:24:33 +0100

    carousel finish

--- src/app/home/home-carousel-enfant/home-carousel-enfant.component.ts
+++ src/app/home/home-carousel-enfant/home-carousel-enfant.component.ts
@@ -5,15 +5,17 @@ import { HttpClient } from '@angular/common/http';
   selector: 'app-home-carousel-enfant',
   templateUrl: './home-carousel-enfant.component.html',
   styleUrls: ['./home-carousel-enfant.component.css'],
-  animations: [
-
-  ]
 })
+
 export class HomeCarouselEnfantComponent implements OnInit {
 
   images: any[];
   descriptions: any [];
   categories: any[];
+
+  marginCaroussel = 0;
+  nbMovie = 0;
+  widthContent = 0;
 
   constructor(private http: HttpClient) { }
 
@@ -23,6 +25,26 @@ export class HomeCarouselEnfantComponent implements OnInit {
     this.http.get('http://127.0.0.1:8006/api/movies')
       .subscribe( (response: any) => {
         this.descriptions = response['hydra:member'];
+        this.nbMovie = this.descriptions.length;
+        console.log('nbMovie = this.descriptions.length', this.nbMovie);
     });
+  }
+
+  moveLeft() {
+    this.marginCaroussel += 400;
+
+    if (this.marginCaroussel > 0) {
+      this.marginCaroussel = 0;
+    }
+  }
+
+  moveRight() {
+    this.marginCaroussel -= 400;
+
+    this.widthContent = (330 * this.nbMovie);
+
+    if (this.marginCaroussel < -this.widthContent) {
+      this.marginCaroussel = -this.widthContent + 330;
+    }
   }
 }
